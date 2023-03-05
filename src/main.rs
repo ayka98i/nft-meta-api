90cd0046f0095806d8b8b3e67b49e16fcf89723f
ayka98i <ayka98i@dmail.ai>
2023-03-05 18:06:27 +0900

    add: ping route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{guard, http, web, App, HttpResponse, HttpServer};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file("./local_keys/key.pem", SslFiletype::PEM)
+        .unwrap();
+    builder
+        .set_certificate_chain_file("./local_keys/cert.pem")
+        .unwrap();
+
+    HttpServer::new(|| App::new().configure(ping_config))
+        .bind_openssl("localhost:8080", builder)?
+        .run()
+        .await
+}
+
+fn ping_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/ping")
+            .guard(guard::Method(http::Method::GET))
+            .route(web::get().to(|| async { HttpResponse::Ok().body("pong") })),
+    );
 }
